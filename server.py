9ee21b5585ef06da390899c537fc6a179de4639e
max <max.o.harris@outlook.com>
2021-05-05 12:48:37 +0100

    init

---
+++ server.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3.6
+
+import json
+import os
+import random
+import uuid
+from time import strftime, gmtime
+
+from waitress import serve
+from flask import Flask, jsonify, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_index():
+    return redirect("https://fishwelfareinitiative/", code=302)
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify(
+        status="ok"
+    )
+    
+if __name__ == '__main__':
+    serve(app, port=8080)
